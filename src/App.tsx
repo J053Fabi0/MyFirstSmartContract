089d10eb134b3a2fd9c5621733b4633672535810
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-07-07 14:34:30 -0600

    Remove unused div

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <TonConnectButton />
-      </div>
+      <TonConnectButton />
       <div>
         <div className="Card">
           <b>Our contract Address</b>
